5a4ac89be0a12a6b4b85a00fb688910cf7c7a6ed
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2021-12-09 20:14:17 +0200

    add multiply and divide functions

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,9 +4,15 @@ int main()
 {
     std::cout << "Hello World!\n";
 }
-int Plus(int a, int b) {
+int plus(int a, int b) {
     return a + b;
 }
-int Minus(int a, int b) {
+int minus(int a, int b) {
     return a - b;
+}
+int multiply(int a, int b) {
+    return a * b;
+}
+int divide(int a, int b) {
+    return a / b;
 }
